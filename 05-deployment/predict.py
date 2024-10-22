822dfb4561a73c1077cba4f17ef515340eb38d4a
Armand Winant <bastien.winant@mail.mcgill.ca>
2024-10-22 21:47:05 +0200

    create a churn prediction web service

--- 05-deployment/predict.py
+++ 05-deployment/predict.py
@@ -1,6 +1,6 @@
-from sklearn.feature_extraction import DictVectorizer
-from sklearn.linear_model import LogisticRegression
 import pickle
+from flask import Flask, request, jsonify
+
 
 C = 1.0
 
@@ -9,30 +9,22 @@ input_file = f'model_C={C}.bin'
 with open(input_file, 'rb') as f_in:
   dv, model = pickle.load(f_in)
 
-customer = {
-    'gender': 'female',
-    'seniorcitizen': 0,
-    'partner': 'yes',
-    'dependents': 'no',
-    'phoneservice': 'no',
-    'multiplelines': 'no_phone_service',
-    'internetservice': 'dsl',
-    'onlinesecurity': 'no',
-    'onlinebackup': 'yes',
-    'deviceprotection': 'no',
-    'techsupport': 'no',
-    'streamingtv': 'no',
-    'streamingmovies': 'no',
-    'contract': 'month-to-month',
-    'paperlessbilling': 'yes',
-    'paymentmethod': 'electronic_check',
-    'tenure': 1,
-    'monthlycharges': 29.85,
-    'totalcharges': 29.85
-}
+app = Flask('churn')
 
-X = dv.transform([customer])
-y_pred = model.predict_proba(X)[0, 1]
+@app.route('/predict', methods=['POST'])
+def predict():
+  customer = request.get_json()
 
-print(f'Input data: {customer}')
-print(f'Model prediction probability: {y_pred}')
+  X = dv.transform([customer])
+  y_pred = model.predict_proba(X)[0, 1]
+  churn = y_pred >= .5
+
+  response = {
+    'churn_probability': float(y_pred),
+    'churn': bool(churn)
+  }
+
+  return jsonify(response)
+
+if __name__=="__main__":
+  app.run(debug=True, host='0.0.0.0', port=9696)
